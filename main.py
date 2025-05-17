f4f6b55c27551039b4586aa888605a1248d51ebc
aniketpatil1551 <patilaniket0185@gmail.com>
2025-05-17 08:24:24 +0530

    Modified main.py file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 
 fruits = ['apple', 'blueberry', 'cherry', 'orange']
-counts = [140, 10, 30, 55]
+counts = [140, 20, 30, 55]
 bar_labels = ['red', 'blue', '_red', 'orange']
 bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
 
